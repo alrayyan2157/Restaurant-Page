d7a0fc7c2d40b946fc5cc3fe4581cb16f5bd8f33
alrayyan2157 <alrayyan2157@icloud.com>
2026-06-15 07:29:41 +0530

    working content

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("testing");
+import { loadHome } from "./home.js";
+import { loadMenu } from "./menu.js";
+import { loadContact } from "./contact.js";
+
+const contentDiv = document.getElementById("content");
+const homeBtn = document.getElementById("home-btn");
+const menuBtn = document.getElementById("menu-btn");
+const contactBtn = document.getElementById("contact-btn");
+
+function renderTab(tabElement) {
+    contentDiv.textContent = "";
+    contentDiv.appendChild(tabElement);
+}
+
+homeBtn.addEventListener("click", () => renderTab(loadHome()));
+menuBtn.addEventListener("click", () => renderTab(loadMenu()));
+contactBtn.addEventListener("click", () => renderTab(loadContact()));
+
+renderTab(loadHome());
